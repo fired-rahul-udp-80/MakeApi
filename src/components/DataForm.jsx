534a19d305f1ca6f882232a3c62840ea44208a71
Rahul Kumar <152783083+fired-rahul-udp-80@users.noreply.github.com>
2024-12-28 23:03:29 +0530

    Update DataForm.jsx

--- src/components/DataForm.jsx
+++ src/components/DataForm.jsx
@@ -34,7 +34,7 @@ const DataForm = ({ schema, makeApiLink ,setApiId, setStep,theme }) => {
   };
 
   return (
-    <div className={`container mx-auto p-4 bg-white shadow rounded ${theme == "light" ? "bg-slate-900 backdrop-blur-md" : " dark:bg-gray-800"} dark:text-white`}>
+    <div className={`container mx-auto p-4 bg-white shadow rounded ${theme == "light" ? "dark:bg-slate-900 backdrop-blur-md" : " dark:bg-gray-800"} dark:text-white`}>
       <h2 className="text-xl font-bold mb-4">Enter Data for Schema</h2>
       {schema.map((field, index) => (
         <input
